5fc259c40d767d5d085d3d195416e3e214e8e0e5
Vlad <novlad26@gmail.com>
2018-03-31 06:22:14 +0300

    Wrote adding dialog without making new string in table

--- src/mysearchjavafx/MySearchJavaFX.java
+++ src/mysearchjavafx/MySearchJavaFX.java
@@ -7,6 +7,7 @@ package mysearchjavafx;
 
 //import java.util.Optional;
 import java.util.ArrayList;
+import java.lang.Integer;
 
 import javafx.application.*;
 //import javafx.geometry.Pos;
@@ -29,7 +30,7 @@ public class MySearchJavaFX extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        ArrayList<Student> arrayAboutStudent = new ArrayList();
+        ArrayList<Student> studentsArray = new ArrayList();
         
         TableView<StudentTableClass> curentTable = new TableView();     
         TableColumn studentsName = new TableColumn("Students full name");
@@ -133,56 +134,108 @@ public class MySearchJavaFX extends Application {
             
             Stage curentDialogStage = new Stage();
             
+            ArrayList<HBox> HBoxesOfCurrentDialog = new ArrayList();
+            
             Label studentFirstNameLabel = new Label("Student first name");
             TextField studentFirstNameTextField = new TextField();        
-            HBox curentDialogHBox1 = makeNewHBox(studentFirstNameLabel, studentFirstNameTextField, 63);
+            HBoxesOfCurrentDialog.add(makeNewHBox(studentFirstNameLabel, studentFirstNameTextField, 63));
             
             Label studentSurNameLabel = new Label("Student sur name");
             TextField studentSurNameTextField = new TextField();
-            HBox curentDialogHBox2 = makeNewHBox(studentSurNameLabel, studentSurNameTextField, 67);
+            HBoxesOfCurrentDialog.add(makeNewHBox(studentSurNameLabel, studentSurNameTextField, 67));
             
             Label studentLastNameLabel = new Label("Student last name");
             TextField studentLastNameTextField = new TextField();
-            HBox curentDialogHBox3 = makeNewHBox(studentLastNameLabel, studentLastNameTextField, 65);
+            HBoxesOfCurrentDialog.add(makeNewHBox(studentLastNameLabel, studentLastNameTextField, 65));
             
-            Label fatherFirstNameLabel = new Label("Father last Name");
-            TextField fatherFirstNameTtextField = new TextField();
-            HBox curentDialogHBox4 = makeNewHBox(fatherFirstNameLabel, fatherFirstNameTtextField, 71);
+            Label fatherFirstNameLabel = new Label("Father first name");
+            TextField fatherFirstNameTextField = new TextField();        
+            HBoxesOfCurrentDialog.add(makeNewHBox(fatherFirstNameLabel, fatherFirstNameTextField, 71));
+            
+            Label fatherSurNameLabel = new Label("Father sur name");
+            TextField fatherSurNameTextField = new TextField();
+            HBoxesOfCurrentDialog.add(makeNewHBox(fatherSurNameLabel, fatherSurNameTextField, 75));
+            
+            Label fatherLastNameLabel = new Label("Father last Name");
+            TextField fatherLastNameTextField = new TextField();
+            HBoxesOfCurrentDialog.add(makeNewHBox(fatherLastNameLabel, fatherLastNameTextField, 71));
+            
+            Label fatherSalaryRublesLabel = new Label("Father salary, rubles");
+            TextField fatherSalaryRublesTextField = new TextField();
+            HBoxesOfCurrentDialog.add(makeNewHBox(fatherSalaryRublesLabel, fatherSalaryRublesTextField, 57));
+            
+            Label fatherSalaryPennyLabel = new Label("Father salary, penny");
+            TextField fatherSalaryPennyTextField = new TextField();
+            HBoxesOfCurrentDialog.add(makeNewHBox(fatherSalaryPennyLabel, fatherSalaryPennyTextField, 57));
+            
+            Label motherFirstNameLabel = new Label("Mother first name");
+            TextField motherFirstNameTextField = new TextField();        
+            HBoxesOfCurrentDialog.add(makeNewHBox(motherFirstNameLabel, motherFirstNameTextField, 67));
+            
+            Label motherSurNameLabel = new Label("Mother sur name");
+            TextField motherSurNameTextField = new TextField();
+            HBoxesOfCurrentDialog.add(makeNewHBox(motherSurNameLabel, motherSurNameTextField, 71));
+            
+            Label motherLastNameLabel = new Label("Mother last Name");
+            TextField motherLastNameTextField = new TextField();
+            HBoxesOfCurrentDialog.add(makeNewHBox(motherLastNameLabel, motherLastNameTextField, 67));
+            
+            Label motherSalaryRublesLabel = new Label("Mother salary, rubles");
+            TextField motherSalaryRublesTextField = new TextField();
+            HBoxesOfCurrentDialog.add(makeNewHBox(motherSalaryRublesLabel, motherSalaryRublesTextField, 53));
+            
+            Label motherSalaryPennyLabel = new Label("Mother salary, penny");
+            TextField motherSalaryPennyTextField = new TextField();
+            HBoxesOfCurrentDialog.add(makeNewHBox(motherSalaryPennyLabel, motherSalaryPennyTextField, 51));
+            
+            Label numberOfBrothersLabel = new Label("Number of student brothers");
+            TextField numberOfBrothersTextField = new TextField();
+            HBoxesOfCurrentDialog.add(makeNewHBox(numberOfBrothersLabel, numberOfBrothersTextField, 13));
+            
+            Label numberOfSistersLabel = new Label("Number of student sisters");
+            TextField numberOfSistersTextField = new TextField();
+            HBoxesOfCurrentDialog.add(makeNewHBox(numberOfSistersLabel, numberOfSistersTextField, 25));
             
             VBox curentDialogVBox = new VBox();
             curentDialogVBox.setSpacing(10);
             curentDialogVBox.setPadding(new Insets(15,20,10,10));
-            curentDialogVBox.getChildren().addAll(
-                    curentDialogHBox1, 
-                    curentDialogHBox2, 
-                    curentDialogHBox3, 
-                    curentDialogHBox4
-            );
+            for(int iter = 0; iter < HBoxesOfCurrentDialog.size(); iter++)
+                curentDialogVBox.getChildren().add(HBoxesOfCurrentDialog.get(iter));
             
             Button curentDialogOk = new Button("OK");
             curentDialogOk.setOnAction(action ->{
-                /*String studentFirstName = studentFirstNameTextField.getText();
+                String studentFirstName = studentFirstNameTextField.getText();
                 String studentSurName = studentSurNameTextField.getText();
                 String studentLastName = studentLastNameTextField.getText();
-                PeopleFio studentFio = new PeopleFio(studentFirstName, studentSurName, studentLastName);
-                PeopleFio fatherFio = new PeopleFio(studentFirstName, studentSurName, studentLastName);
-                PeopleFio motherFio = new PeopleFio(studentFirstName, studentSurName, studentLastName);
-                MoneyBr fatherSalary = new MoneyBr(12,12);
-                MoneyBr motherSalary = new MoneyBr(12,12);
-                int numberOfBrothers = 2;
-                int numberOfSisters = 2;
-                AboutStudent informationAboutNewStudent = new AboutStudent(
-                        studentFio,
-                        fatherFio,
-                        fatherSalary,
-                        motherFio,
-                        motherSalary,
-                        numberOfBrothers,
-                        numberOfSisters
+                String fatherFirstName = fatherFirstNameTextField.getText();
+                String fatherSurName = fatherSurNameTextField.getText();
+                String fatherLastName = fatherLastNameTextField.getText();
+                int fatherSalaryRubles = Integer.parseInt(fatherSalaryRublesTextField.getText());
+                int fatherSalaryPenny = Integer.parseInt(fatherSalaryPennyTextField.getText());
+                MoneyBr fSalary = new MoneyBr(fatherSalaryRubles,fatherSalaryPenny);
+                String motherFirstName = motherFirstNameTextField.getText();
+                String motherSurName = motherSurNameTextField.getText();
+                String motherLastName = motherLastNameTextField.getText();
+                int motherSalaryRubles = Integer.parseInt(motherSalaryRublesTextField.getText());
+                int motherSalaryPenny = Integer.parseInt(motherSalaryPennyTextField.getText());  
+                MoneyBr mSalary = new MoneyBr(motherSalaryRubles,motherSalaryPenny);
+                Parent father = new Parent(studentFirstName, studentSurName, studentLastName, fSalary);
+                Parent mother = new Parent(studentFirstName, studentSurName, studentLastName, mSalary);
+                int numOfBrothers = Integer.parseInt(numberOfBrothersTextField.getText());
+                int numOfSisters = Integer.parseInt(numberOfSistersTextField.getText());       
+                Student informationAboutNewStudent = new Student(
+                        studentFirstName,
+                        studentSurName,
+                        studentLastName,
+                        father,
+                        mother,
+                        numOfBrothers,
+                        numOfSisters
                 );
-                arrayAboutStudent.add(informationAboutNewStudent);
+                studentsArray.add(informationAboutNewStudent);
+                
                 System.out.println("All right");
-                curentDialogStage.close();*/
+                curentDialogStage.close();
             });
             
             Button curentDialogCancel = new Button("Cancel");
@@ -199,14 +252,12 @@ public class MySearchJavaFX extends Application {
             AnchorPane.setRightAnchor(curentDialogHBoxWithButtons, 10.0);
             curentDialogAnchorPane.getChildren().addAll(curentDialogVBox, curentDialogHBoxWithButtons);
             
-            Scene curentDialogScene = new Scene(curentDialogAnchorPane, 355, 250);
+            Scene curentDialogScene = new Scene(curentDialogAnchorPane, 355, 600);
             
             curentDialogStage.initStyle(StageStyle.UTILITY);
             curentDialogStage.setTitle("Information about new student");
             curentDialogStage.setScene(curentDialogScene);
-            curentDialogStage.show();
-
-            
+            curentDialogStage.show();  
         });
         
         Button toolDeleteButton = new Button();
